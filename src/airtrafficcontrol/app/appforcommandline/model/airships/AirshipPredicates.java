6f93664c0725efdc28b9ce33927696332e84c661
Daniel Gomes <danielacgomes1992@gmail.com>
2014-12-13 23:51:32 +0000

    Update of InMemoryAirshipDatabaseTests

--- src/airtrafficcontrol/app/appforcommandline/model/airships/AirshipPredicates.java
+++ src/airtrafficcontrol/app/appforcommandline/model/airships/AirshipPredicates.java
@@ -6,30 +6,30 @@ import java.util.function.Predicate;
  * Class which static subclasses implement {@link Predicate}
  * 
  *
- *@author Daniel Gomes, Eva Gomes, Gonçalo Carvalho, Pedro Antunes
+ * @author Daniel Gomes, Eva Gomes, Gonçalo Carvalho, Pedro Antunes
  */
-public class AirshipPredicates
-{
+public class AirshipPredicates {
+
 	/**
-	 * Predicate for {@link CivilAirship} 
+	 * Predicate for {@link CivilAirship}
 	 * 
 	 *
-	 *@author Daniel Gomes, Eva Gomes, Gonçalo Carvalho, Pedro Antunes
+	 * @author Daniel Gomes, Eva Gomes, Gonçalo Carvalho, Pedro Antunes
 	 */
-	public static class IsBelowPassagerNumber implements Predicate<Airship>
-	{
+	public static class IsBelowPassagerNumber implements Predicate<Airship> {
+
 		private final double passengerNumber;
 
-		public IsBelowPassagerNumber( double passengerNumber )
-		{
+		public IsBelowPassagerNumber(double passengerNumber) {
+
 			this.passengerNumber = passengerNumber;
 		}
 
 		@Override
-		public boolean test( Airship airship )
-		{
-			if( airship instanceof CivilAirship)
-				return ((CivilAirship)airship).getPassengers() > passengerNumber;
+		public boolean test(Airship airship) {
+
+			if (airship instanceof CivilAirship)
+				return ((CivilAirship) airship).getPassengers() > passengerNumber;
 			else
 				return false;
 		}
--- src/airtrafficcontrol/tests/testsforcommandline/InMemoryAirshipDatabaseTests.java
+++ src/airtrafficcontrol/tests/testsforcommandline/InMemoryAirshipDatabaseTests.java
@@ -2,18 +2,26 @@ package airtrafficcontrol.tests.testsforcommandline;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import airtrafficcontrol.app.appforcommandline.model.Database;
+
+import airtrafficcontrol.app.appforcommandline.exceptions.databaseexceptions.NoSuchElementInDatabaseException;
 import airtrafficcontrol.app.appforcommandline.model.airships.Airship;
+import airtrafficcontrol.app.appforcommandline.model.airships.AirshipPredicates;
+import airtrafficcontrol.app.appforcommandline.model.airships.CivilAirship;
 import airtrafficcontrol.app.appforcommandline.model.airships.InMemoryAirshipDatabase;
 import airtrafficcontrol.app.appforcommandline.model.airships.MilitaryAirship;
 import airtrafficcontrol.app.appforcommandline.model.users.User;
 
 public class InMemoryAirshipDatabaseTests {
 
-	private Database<Airship> airshipDatabase;
-	private Airship airship;
+	private InMemoryAirshipDatabase airshipDatabase;
+	private Airship airship, airship2;
 	private User user;
 
 	@Before
@@ -36,7 +44,7 @@ public class InMemoryAirshipDatabaseTests {
 	public void shouldAddSecondMilitaryAirshipByTheSameUser() {
 
 		// Arrange
-		Airship airship2 = new MilitaryAirship(0, 0, 0, 10, 0, false);
+		airship2 = new MilitaryAirship(0, 0, 0, 10, 0, false);
 
 		// Assert
 		assertTrue(airshipDatabase.add(airship, user));
@@ -46,25 +54,42 @@ public class InMemoryAirshipDatabaseTests {
 	@Test
 	public void shouldNotAddTheSameMilitaryAirship() {
 
+		// Act
 		airshipDatabase.add(airship, user);
+
 		// Assert
 		assertFalse(airshipDatabase.add(airship, user));
 	}
 
-	@Test
-	public void shouldReturnFalseCheckingIfThisAirshipIsInCorridor() {
+	@Test (expected = NoSuchElementInDatabaseException.class)
+	public void shouldThrowNoSuchElementInDatabaseExceptionWhenTryingToFindNonExistingAirship()
+			throws NoSuchElementInDatabaseException {
 
+		// Act
 		airshipDatabase.add(airship, user);
+
+		// Assert
+		assertFalse(((InMemoryAirshipDatabase) airshipDatabase).checkIfThisAirshipIsInCorridor("5"));
+	}
+
+	@Test
+	public void shouldReturnFalseCheckingIfThisAirshipIsInCorridor()
+			throws NoSuchElementInDatabaseException {
+
+		// Act
+		airshipDatabase.add(airship, user);
+
 		// Assert
 		assertFalse(((InMemoryAirshipDatabase) airshipDatabase)
 				.checkIfThisAirshipIsInCorridor(airship.getIdentification()));
 	}
 
 	@Test
-	public void shouldReturnTrueCheckingIfThisAirshipIsInCorridor() {
+	public void shouldReturnTrueCheckingIfThisAirshipIsInCorridor()
+			throws NoSuchElementInDatabaseException {
 
 		// Arrange
-		Airship airship2 = new MilitaryAirship(0, 0, 0, 10, 5, false);
+		airship2 = new MilitaryAirship(0, 0, 0, 10, 5, false);
 
 		// Act
 		airshipDatabase.add(airship2, user);
@@ -72,5 +97,65 @@ public class InMemoryAirshipDatabaseTests {
 		// Assert
 		assertTrue(((InMemoryAirshipDatabase) airshipDatabase)
 				.checkIfThisAirshipIsInCorridor(airship2.getIdentification()));
+	}
+
+	@Test
+	public void shouldGetAllAirshipsRegistedByTheSameUser() {
+
+		// Arrange
+		airship2 = new MilitaryAirship(0, 0, 0, 10, 5, false);
+		List<Airship> airships = new ArrayList<>();
+
+		// Act
+		airshipDatabase.add(airship, user);
+		airshipDatabase.add(airship2, user);
+
+		airships.add(airship);
+		airships.add(airship2);
+
+		// Assert
+		Assert.assertEquals(airshipDatabase.getAirshipsOfUser("X").toString(), airships.toString());
+	}
+
+	@Test
+	public void shouldGetAllAirshipsThatAreTransgressing() {
+
+		// Arrange
+		airship2 = new MilitaryAirship(0, 0, 0, 10, 5, false);
+		List<Airship> airships = new ArrayList<>();
+
+		// Act
+		airshipDatabase.add(airship, user);
+		airshipDatabase.add(airship2, user);
+
+		airships.add(airship2);
+
+		// Assert
+		Assert.assertEquals(airshipDatabase.reportTransgressions().toString(), airships.toString());
+	}
+
+	@Test
+	public void shouldGetAllAirshipsThatVerifyACertainCriteria() {
+
+		// Arrange
+		airship2 = new CivilAirship(0, 0, 0, 10, 5, 100);
+		Airship airship3 = new CivilAirship(0, 0, 0, 10, 5, 2);
+		Airship airship4 = new CivilAirship(0, 0, 0, 10, 5, 200);
+		
+		List<Airship> airships = new ArrayList<>();
+
+		// Act
+		airshipDatabase.add(airship, user);
+		airshipDatabase.add(airship2, user);
+		airshipDatabase.add(airship3, user);
+		airshipDatabase.add(airship4, user);
+
+		airships.add(airship2);
+		airships.add(airship3);
+
+		// Assert
+		Assert.assertEquals(
+				airshipDatabase.getAirshipsThat(new AirshipPredicates.IsBelowPassagerNumber(102))
+						.toString(), airships.toString());
 	}
 }

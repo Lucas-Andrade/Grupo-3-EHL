ab225dad05f205016e6bdd692b83b25affe45fd7
Daniel Gomes <danielacgomes1992@gmail.com>
2014-12-14 00:47:10 +0000

    Added an Override of the toString method to CivilAirship and MilitaryAirship.

--- src/airtrafficcontrol/tests/testsforcommandline/GetAirshipByIdCommand_Tests.java
+++ src/airtrafficcontrol/tests/testsforcommandline/GetAirshipByIdCommand_Tests.java
@@ -49,7 +49,7 @@ public class GetAirshipByIdCommand_Tests {
 
 		"Flight ID: 1\nLatitude: 10.0 Longitude: 10.0 Altitude: 1000.0"
 				+ "\nMaximum Altitude Permited: 2000.0 Minimum Altitude Permited: 200.0"
-				+ "\nIs Outside The Given Corridor: false");
+				+ "\nIs Outside The Given Corridor: false\nCarries Weapons: false");
 	}
 
 	@Test

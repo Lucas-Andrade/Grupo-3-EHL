2f3a815dda4b9281797f62e6d2016bfc1fe7a3c5
E.v.a.aA.a <eva@eva.pt>
2015-01-06 17:53:37 +0000

    - updated Translatable's documentation - completed IterableAirshipConversor and IterableUserConversor

--- src/main/java/cli/outputformatters/totranslatableconversors/IterableAirshipConversor.java
+++ src/main/java/cli/outputformatters/totranslatableconversors/IterableAirshipConversor.java
@@ -1,15 +1,43 @@
 package main.java.cli.outputformatters.totranslatableconversors;
 
+import java.util.HashMap;
+import java.util.Map;
+import main.java.cli.model.airships.Airship;
 import main.java.cli.outputformatters.Translatable;
+import main.java.cli.utils.exceptions.conversorsexceptions.UnknownTypeException;
 
-
+/**
+ * Class whose instances convert instances of {@code Iterable<Airship>} into
+ * {@link Translatables}.
+ *
+ * @author Daniel Gomes, Eva Gomes, Gonçalo Carvalho, Pedro Antunes
+ */
 public class IterableAirshipConversor extends Conversor
 {
 	
+	@SuppressWarnings( "unchecked" )
 	@Override
-	Translatable convert( Object object ) {
-		// TODO Auto-generated method stub
-		return null;
+	Translatable convert( Object iterableOfAirships ) throws UnknownTypeException {
+
+		Iterable< Airship > it;
+		try
+		{
+			it = (Iterable< Airship >)iterableOfAirships;
+		}
+		catch( ClassCastException e )
+		{
+			throw new UnknownTypeException( "Could not convert " + iterableOfAirships
+					+ " into a translatable." );
+		}
+		
+		Map< String, Object > propertiesBag = new HashMap< String, Object >();		
+		for( Airship user : it )
+		{
+			propertiesBag.put( user.getIdentification(),
+					ToTranslatableConversor.convert( user ) );
+		}
+		
+		return new Translatable( "users", "user", null, null, propertiesBag, null );
 	}
 	
 }

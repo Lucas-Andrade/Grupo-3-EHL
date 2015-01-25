50631161143524404559374bc0ccb4eb6f307119
E.v.a.aA.a <eva@eva.pt>
2015-01-25 16:28:19 +0000

    - updated the parametersMap given to factories to be unmodifiable (in method internalGetCommand of class CommandParser) - ctrl+shift+f in InvalidArgumentException - corrected messages of the exceptions thrown by the constructor of the GeographicCoordinate class

--- src/main/java/utils/exceptions/InvalidArgumentException.java
+++ src/main/java/utils/exceptions/InvalidArgumentException.java
@@ -8,25 +8,21 @@ package main.java.utils.exceptions;
  * @author Daniel Gomes, Eva Gomes, Gonçalo Carvalho, Pedro Antunes
  */
 @SuppressWarnings( "serial" )
-public class InvalidArgumentException extends Exception
-{
-	
-	/**
-	 * Constructs an {@link InvalidArgumentException} with no detail message.
-	 */
-	public InvalidArgumentException() {
-		
-	}
-	
-	/**
-	 * Constructs an {@link InvalidArgumentException} with the specified detail
-	 * message.
-	 * 
-	 * @param message
-	 *            The detail message.
-	 */
-	public InvalidArgumentException( String message ) {
-		
-		super( message );
-	}
+public class InvalidArgumentException extends Exception {
+    
+    /**
+     * Constructs an {@link InvalidArgumentException} with no detail message.
+     */
+    public InvalidArgumentException() {}
+    
+    /**
+     * Constructs an {@link InvalidArgumentException} with the specified detail message.
+     * 
+     * @param message
+     *            The detail message.
+     */
+    public InvalidArgumentException( String message ) {
+        
+        super( message );
+    }
 }

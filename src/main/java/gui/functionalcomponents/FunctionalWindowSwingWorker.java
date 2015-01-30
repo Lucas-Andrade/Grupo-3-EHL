cec542af1cb39c7fe563d38181cb6e58afdaffbb
gonçalo <gonalo@RT>
2015-01-30 16:37:08 +0000

    Documentation...

--- src/main/java/gui/functionalcomponents/FunctionalWindowSwingWorker.java
+++ src/main/java/gui/functionalcomponents/FunctionalWindowSwingWorker.java
@@ -8,8 +8,9 @@ import javax.swing.SwingWorker;
 
 /**
  * Abstract SwingWorker Class that {@code Override} the {@code done()} method,
- * where the {@link Exception}s thrown in the doInBackground() are caught and
- * write on the {@code errorLabel}.
+ * where the {@link Exception}s thrown by the
+ * {@link SwingWorker#doInBackground()} method are caught and write on the
+ * {@code errorLabel}.
  */
 public abstract class FunctionalWindowSwingWorker< T >
 	extends SwingWorker< T, Void >
@@ -21,6 +22,12 @@ public abstract class FunctionalWindowSwingWorker< T >
 		this.errorLabel = errorLabel;
 	}
 
+	/**
+	 * Caught and write the thrown {@link Exception}s by the
+	 * {@link SwingWorker#doInBackground()} or
+	 * {@link FunctionalWindowSwingWorker#functionalDone} methods to the
+	 * {@code errorLabel}.
+	 */
 	@Override
 	final protected void done()
 	{
@@ -40,8 +47,8 @@ public abstract class FunctionalWindowSwingWorker< T >
 	}
 
 	/**
-	 * Contract for subclasses where the result of the {@code doInBackground()} method 
-	 * will be treated. Implementations decisions: The treatment of
+	 * Contract for subclasses where the result of the {@code doInBackground()}
+	 * method will be treated. Implementations decisions: The treatment of
 	 * {@link Exception}s are caught in the
 	 * {@link FunctionalWindowSwingWorker#done()} method.
 	 * 
--- src/main/java/gui/functionalcomponents/functionalairshipwindows/FunctionalGetGeographicalCoordinatesParametersWindow.java
+++ src/main/java/gui/functionalcomponents/functionalairshipwindows/FunctionalGetGeographicalCoordinatesParametersWindow.java
@@ -8,7 +8,7 @@ import main.java.domain.model.airships.Airship;
 import main.java.domain.model.airships.InMemoryAirshipsDatabase;
 import main.java.gui.design.panels.mainwindowpanels.JBodyPanelForMainWindow;
 import main.java.gui.design.windows.airshipwindows.GetGeographicalCoordinatesParametersWindow;
-import main.java.gui.functionalcomponents.FunctionalGetSwingWorker;
+import main.java.gui.functionalcomponents.FunctionalGetWindowSwingWorker;
 import main.java.gui.functionalcomponents.FunctionalWindow;
 import main.java.gui.functionalcomponents.FunctionalWindowSwingWorker;
 import main.java.utils.StringUtils;
@@ -34,7 +34,7 @@ public class FunctionalGetGeographicalCoordinatesParametersWindow
 	@Override
 	protected FunctionalWindowSwingWorker< Iterable< Airship >> getSwingWorker()
 	{
-		return new FunctionalGetSwingWorker( airshipsDatabase, bodyPanel, functionalWindow.getErrorLabel() )
+		return new FunctionalGetWindowSwingWorker( airshipsDatabase, bodyPanel, functionalWindow.getErrorLabel() )
 		{
 			String latitude = functionalWindow.getLatitude().getJTextField().getText();
 			String longitude = functionalWindow.getLongitude().getJTextField().getText();

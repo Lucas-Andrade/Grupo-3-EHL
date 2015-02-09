c0244f6e312d4a3e8a50c348d8dde0f15c7bf4eb
Gonçalo <Gonalo@G-PC>
2015-02-09 11:22:32 +0000

    Change GUser

--- air-traffic-control-applications/air-traffic-control-applications-distributed/air-traffic-control-applications-distributed-client/src/main/java/gson_entities/GUser.java
+++ air-traffic-control-applications/air-traffic-control-applications-distributed/air-traffic-control-applications-distributed-client/src/main/java/gson_entities/GUser.java
@@ -1,11 +1,13 @@
 package gson_entities;
 
+import entities.SimpleUser;
+
 
 public class GUser {
     
     private String username;
     private String email;
-    private String fullName;
+    private String fullname;
     
     public String toString() {
     
@@ -13,8 +15,14 @@ public class GUser {
         
         result.append( username ).append( ",\r\nemail: " ).append( email );
         
-        return fullName.equals( null ) ? result.append( "\r\n" ).toString()
-                                    : result.append( ",\r\nfullName: " ).append( fullName )
+        return fullname.equals( null ) ? result.append( "\r\n" ).toString()
+                                    : result.append( ",\r\nfullName: " ).append( fullname )
                                             .append( "\r\n" ).toString();
+    }
+    
+    
+    public SimpleUser convert()
+    {
+        return new SimpleUser( username, email, fullname, toString() );
     }
 }

e042e69c4e01b705159e355c584e48df0d5934cd
pantunes1 <pedrojosesrantunes@gmail.com>
2015-02-12 20:45:11 +0000

    commit using eclipse

--- atc-apps-graphics_components/src/main/java/functionalcomponents/functionalairshipwindows/FunctionalGetAirshipsWithLessPassengerThanWindow.java
+++ atc-apps-graphics_components/src/main/java/functionalcomponents/functionalairshipwindows/FunctionalGetAirshipsWithLessPassengerThanWindow.java
@@ -2,129 +2,154 @@ package functionalcomponents.functionalairshipwindows;
 
 
 
-import java.awt.event.ActionListener;
-import javax.swing.SwingWorker;
 import swingworkers.ExceptionHandlerSW;
-import swingworkers.FunctionalGetWindowSwingWorker;
+import swingworkers.SwingWorkerFactory;
 import design.panels.mainwindowpanels.JBodyPanelForMainWindow;
 import design.windows.airshipwindows.GetAirshipsWithLessPassengerThanWindow;
+import entities.SimpleAirship;
+import exceptions.SwingWorkerFactoryMissingException;
 import functionalcomponents.FunctionalWindow;
 
 
-
 /**
- * Class whose instances have the purpose of add functionality to a
- * {@link GetAirshipsWithLessPassengerThanWindow}. Giving functionality to a window means adding an
- * {@link ActionListener} to all its buttons.
- *
- * Extends {@link FunctionalWindow} of {@link Iterable} of {@link Airship}.
+ * Class whose instances have the purpose of update {@link JBodyPanelForMainWindow} 
+ * using for that a {@link SwingWorker}.
+ * 
+ * Extends {@link FunctionalWindow} of {@link Iterable} of {@link SimpleAirship}.
  *
  * @author Daniel Gomes, Eva Gomes, Gonçalo Carvalho, Pedro Antunes
  */
 public class FunctionalGetAirshipsWithLessPassengerThanWindow extends
-        FunctionalWindow< Iterable< Airship > > {
+        FunctionalWindow< FunctionalGetAirshipsWithLessPassengerThanWindow.SwingWorker, Iterable< SimpleAirship > > {
+    
+ // STATIC FIELDS
     
     /**
-     * {@code functionalWindow} - The {@code PatchUserWindow} we want to add functionality to.
+     * The {@link GetAirshipsWithLessPassengerThanWindow} we want to add functionality to.
      */
-    private GetAirshipsWithLessPassengerThanWindow functionalWindow;
+    public static final GetAirshipsWithLessPassengerThanWindow baseWindow =
+            new GetAirshipsWithLessPassengerThanWindow();
     
     /**
-     * {@code airshipsDatabase} - The airships database.
+     * The {@link SwingWorkerFactory} that produces {@link FunctionalGetAirshipsWithLessPassengersThanWindow.SwingWorker}s for
+     * the {@link GetAirshipsWithLessPassengerThanWindow}s.
      */
-    private Database< Airship > airshipsDatabase;
-    
+    private static SwingWorkerFactory< FunctionalGetAirshipsWithLessPassengerThanWindow.SwingWorker, 
+                                                                        Iterable< SimpleAirship > > swFactory;
     /**
-     * {@code bodyPanel} - The central panel of a {@link MainWindow} where the list of airships will
-     * be displayed in a map and in a scroll box.
+     * A lock for the {@link #swFactory}.
      */
-    private JBodyPanelForMainWindow bodyPanel;
+    private static Object factoryLock;
     
+    // CONSTRUCTOR
     /**
-     * Public constructor that will add functionality to a given non functional
-     * {@link GetAirshipsWithLessPassengerThanWindow} and will display it.
-     * 
-     * @param nonFunctionalWindow
-     *            - The {@code GetAirshipsWithLessPassengerThanWindow} we want to add functionality
-     *            to.
-     * @param airshipsDatabase
-     *            - The airships database.
-     * @param bodyPanel
-     *            - The central panel of a {@link MainWindow} where the list of airships will be
-     *            displayed in a map and in a scroll box.
+     * Adds functionality to a {@link GetAirshipsWithLessPassengerThanWindow} and displays it.
      */
-    public FunctionalGetAirshipsWithLessPassengerThanWindow( GetAirshipsWithLessPassengerThanWindow nonFunctionalWindow,
-                                                             Database< Airship > airshipsDatabase,
-                                                             JBodyPanelForMainWindow bodyPanel ) {
-        
-        super( nonFunctionalWindow );
-        
-        this.functionalWindow = nonFunctionalWindow;
-        this.airshipsDatabase = airshipsDatabase;
-        this.bodyPanel = bodyPanel;
+    public FunctionalGetAirshipsWithLessPassengerThanWindow() {
+    
+        super( baseWindow );
     }
     
+    // PUBLIC METHOD
     /**
-     * Method that will return a {@link ExceptionHandlerSW} with an {@code Override}
-     * implementation of its {@link SwingWorker#doInBackground() doInBackground()} and
-     * {@link ExceptionHandlerSW#finalizeDone(Object) functionalDone(Object)} methods to
-     * add the correct functionality to a {@link GetAirshipsWithLessPassengerThanWindow}.
-     * 
-     * @return Returns a {@link ExceptionHandlerSW} with an {@code Override} of its
-     *         methods.
-     */
+     * @see functionalcomponents.FunctionalWindow#getNewSwingWorker()
+     */    
     @Override
-    protected ExceptionHandlerSW< Iterable< Airship >> getNewSwingWorker() {
-        
-        return new FunctionalGetWindowSwingWorker( airshipsDatabase, bodyPanel,
-                                                   functionalWindow.getErrorJTextArea() ) {
-            
-            /**
-             * String representation of the parameters to use in the commands and that are obtained
-             * from the window's text fields.
-             */
-            private String passengersNumber = functionalWindow.getNumberOfPassengers()
-                                                              .getJTextField().getText();
-            
-            /**
-             * Implementation of the {@link SwingWorker#doInBackground() doInBackground()} method
-             * with the purpose the purpose of executing a
-             * {@link GetAirshipsWithLessPassengersThanCommand} and obtaining its result.
-             * 
-             * @return Returns an {@code Iterable<Airship>} containing all the airships that have
-             *         less passengers than the number specified in the window text field.
-             * 
-             * @throws NumberFormatException
-             *             If the String {@code passengersNumber} cannot be converted to Integer.
-             */
-            @Override
-            protected Iterable< Airship > doInBackground() throws Exception {
-                
-                return new GetAirshipsWithLessPassengersThanCommand(
-                                                                     (InMemoryAirshipsDatabase)airshipsDatabase,
-                                                                     StringUtils.parameterToInteger( "Maximum Number of Passengers",
-                                                                                                     passengersNumber ) ).call()
-                                                                                                                         .get();
+    protected SwingWorker getNewSwingWorker() throws SwingWorkerFactoryMissingException {
+    //TODO
+        if( swFactory == null )
+            throw new SwingWorkerFactoryMissingException( this.getClass().getSimpleName() );
+        return swFactory.newInstance();
+    }
+    
+    // STATIC METHOD
+    /**
+     * Sets the {@link SwingWorkerFactory} that produces
+     *  {@link FunctionalGetAirshipsWithLessPassengersThanWindow.SwingWorker}s for the 
+     *  {@link FunctionalGetAirshipsWithLessPassengersThanWindow}s.
+     *   
+     * @param factory
+     *            The {@link SwingWorkerFactory} that produces
+     *            {@link FunctionalGetAirshipsWithLessPassengersThanWindow.SwingWorker}s 
+     *            for the {@link FunctionalGetAirshipsWithLessPassengersThanWindow}s.
+     * @return {@code true} if {@code factory} was set as the factory that produces swingworkers for
+     *         the {@link FunctionalGetAirshipsWithLessPassengersThanWindow}s; <br/>
+     *         {@code false} if there was a factory already set.
+     */
+    public static
+            boolean
+            setSwingWorkerFactory( SwingWorkerFactory< FunctionalGetAirshipsWithLessPassengerThanWindow.SwingWorker,
+                                   Iterable< SimpleAirship > > factory ) {
+        //TODO
+        synchronized (factoryLock) {
+            if( swFactory == null && factory != null ) {
+                swFactory = factory;
+                return true;
             }
-            
-            /**
-             * Override of the {@link FunctionalGetWindowSwingWorker#functionalDone()
-             * functionalDone()}. This method will receive the result of the
-             * {@link SwingWorker#doInBackground() doInBackground()} method and use it to update the
-             * body panel of the {@code MainWindow} and close this window.
-             * 
-             * @param resultOfDoInBackGround
-             *            - The result of the {@link SwingWorker#doInBackground() doInBackground()}
-             *            method.
-             */
-            @Override
-            public final void functionalDone( Iterable< Airship > resultOfDoInBackGround )
-                throws Exception {
-                
-                super.finalizeDone( resultOfDoInBackGround );
-                functionalWindow.dispose();
-            }
-        };
+            return false;
+        }
         
-    };
+    }
+        
+    // INNER CLASS
+    /**
+     * Class whose instances are {@link ExceptionHandlerSW} able to add functionality to a
+     * {@link FunctionalGetAirshipsWithLessPassengersThanWindow}.
+     *
+     * @author Daniel Gomes, Eva Gomes, Gonçalo Carvalho, Pedro Antunes
+     */    
+    public static abstract class SwingWorker extends ExceptionHandlerSW< Iterable< SimpleAirship > > {
+        
+     // INSTANCE FIELDS
+        
+        //TODO
+        private JBodyPanelForMainWindow bodyPanel;
+        
+        /**
+         * String representation of the parameters to use in the commands and that are obtained from
+         * the window's text fields.
+         */
+        
+        // TextField Information
+        protected String passengersNumber;
+    
+        // JLabel Information
+        
+        protected String passengersNumberLabel;
+
+        
+     // CONSTRUCTOR
+        public SwingWorker( GetAirshipsWithLessPassengerThanWindow window ) {
+        
+            super( window.getErrorJTextArea() );
+            
+            passengersNumber = window.getNumberOfPassengers().getJTextField().getText();
+           
+            passengersNumberLabel = window.getNumberOfPassengers().getJLabel().getText();
+        
+        }
+        
+        // IMPLEMENTATION OF METHODS INHERITED FROM SwingWorker and ExceptionHandlerSW
+        /**
+         * Implementation of the {@link ExceptionHandlerSW#finalizeDone }.
+         * This method will receive the result of the
+         * {@link SwingWorker#doInBackground()} method and update 
+         * {@link JBodyPanelForMainWindow}, closing this window.
+         * 
+         * @param resultOfDoInBackGround
+         *            - The result of the {@link SwingWorker#doInBackground()}
+         *            method.
+         */ 
+        
+        @Override
+        protected void finalizeDone( Iterable< SimpleAirship > resultOfDoInBackGround )
+            throws Exception {
+        
+            bodyPanel.updateBodyPanel( resultOfDoInBackGround );
+            
+        }
+        
+    }
+    
+    
 }
--- atc-apps-graphics_components/src/main/java/functionalcomponents/functionalairshipwindows/FunctionalGetGeographicalCoordinatesParametersWindow.java
+++ atc-apps-graphics_components/src/main/java/functionalcomponents/functionalairshipwindows/FunctionalGetGeographicalCoordinatesParametersWindow.java
@@ -1,136 +1,160 @@
 package functionalcomponents.functionalairshipwindows;
 
 
-import Airship;
-import java.awt.event.ActionListener;
-import javax.swing.SwingWorker;
+
 import swingworkers.ExceptionHandlerSW;
-import swingworkers.FunctionalGetWindowSwingWorker;
+import swingworkers.SwingWorkerFactory;
 import design.panels.mainwindowpanels.JBodyPanelForMainWindow;
 import design.windows.airshipwindows.GetGeographicalCoordinatesParametersWindow;
+import entities.SimpleAirship;
+import exceptions.SwingWorkerFactoryMissingException;
 import functionalcomponents.FunctionalWindow;
 
 
-
 /**
- * Class whose instances have the purpose of add functionality to a
- * {@link GetGeographicalCoordinatesParametersWindow}. Giving functionality to a window means adding
- * an {@link ActionListener} to all its buttons.
- *
- * Extends {@link FunctionalWindow} of {@link Iterable} of {@link Airship}.
+ * Class whose instances have the purpose of update {@link JBodyPanelForMainWindow} 
+ * using for that a {@link SwingWorker}.
+ * 
+ * Extends {@link FunctionalWindow} of {@link Iterable} of {@link SimpleAirship}.
  *
  * @author Daniel Gomes, Eva Gomes, Gonçalo Carvalho, Pedro Antunes
  */
 public class FunctionalGetGeographicalCoordinatesParametersWindow extends
-        FunctionalWindow< Iterable< Airship > > {
+        FunctionalWindow< FunctionalGetGeographicalCoordinatesParametersWindow.SwingWorker, Iterable< SimpleAirship > > {
+    
+ // STATIC FIELDS
     
     /**
-     * {@code functionalWindow} - The {@code PatchUserWindow} we want to add functionality to.
+     * The {@link GetGeographicalCoordinatesParametersWindow} we want to add functionality to.
      */
-    private GetGeographicalCoordinatesParametersWindow functionalWindow;
+    public static final GetGeographicalCoordinatesParametersWindow baseWindow =
+            new GetGeographicalCoordinatesParametersWindow();
     
     /**
-     * {@code airshipsDatabase} - The airships database.
+     * The {@link SwingWorkerFactory} that produces {@link FunctionalGetGeographicalCoordinatesParametersWindow.SwingWorker}s for
+     * the {@link GetGeographicalCoordinatesParametersWindow}s.
      */
-    private Database< Airship > airshipsDatabase;
-    
+    private static SwingWorkerFactory< FunctionalGetGeographicalCoordinatesParametersWindow.SwingWorker, 
+                                                                        Iterable< SimpleAirship > > swFactory;
     /**
-     * {@code bodyPanel} - The central panel of a {@link MainWindow} where the list of airships will
-     * be displayed in a map and in a scroll box.
+     * A lock for the {@link #swFactory}.
      */
-    private JBodyPanelForMainWindow bodyPanel;
+    private static Object factoryLock;
     
+    // CONSTRUCTOR
     /**
-     * Public constructor that will add functionality to a given non functional
-     * {@link GetAirshipsWithLessPassengerThanWindow} and will display it.
-     * 
-     * @param nonFunctionalWindow
-     *            - The {@code GetGeographicalCoordinatesParametersWindow} we want to add
-     *            functionality to.
-     * @param airshipsDatabase
-     *            - The airships database.
-     * @param bodyPanel
-     *            - The central panel of a {@link MainWindow} where the list of airships will be
-     *            displayed in a map and in a scroll box.
+     * Adds functionality to a {@link GetGeographicalCoordinatesParametersWindow} and displays it.
      */
-    public FunctionalGetGeographicalCoordinatesParametersWindow( GetGeographicalCoordinatesParametersWindow nonFunctionalWindow,
-                                                                 Database< Airship > airshipsDatabase,
-                                                                 JBodyPanelForMainWindow bodyPanel ) {
-        
-        super( nonFunctionalWindow );
-        
-        this.functionalWindow = nonFunctionalWindow;
-        this.airshipsDatabase = airshipsDatabase;
-        this.bodyPanel = bodyPanel;
+    public FunctionalGetGeographicalCoordinatesParametersWindow() {
+    
+        super( baseWindow );
     }
     
+    // PUBLIC METHOD
     /**
-     * Method that will return a {@link ExceptionHandlerSW} with an {@code Override}
-     * implementation of its {@link SwingWorker#doInBackground() doInBackground()} and
-     * {@link ExceptionHandlerSW#finalizeDone(Object) functionalDone(Object)} methods to
-     * add the correct functionality to a {@link PostUserWindow}.
-     * 
-     * @return Returns a {@link ExceptionHandlerSW} with an {@code Override} of its
-     *         methods.
-     */
+     * @see functionalcomponents.FunctionalWindow#getNewSwingWorker()
+     */    
     @Override
-    protected ExceptionHandlerSW< Iterable< Airship >> getNewSwingWorker() {
+    protected SwingWorker getNewSwingWorker() throws SwingWorkerFactoryMissingException {
+    //TODO
+        if( swFactory == null )
+            throw new SwingWorkerFactoryMissingException( this.getClass().getSimpleName() );
+        return swFactory.newInstance();
+    }
+    
+    // STATIC METHOD
+    /**
+     * Sets the {@link SwingWorkerFactory} that produces
+     *  {@link FunctionalGetGeographicalCoordinatesParametersWindow.SwingWorker}s for the 
+     *  {@link FunctionalGetGeographicalCoordinatesParametersWindow}s.
+     *   
+     * @param factory
+     *            The {@link SwingWorkerFactory} that produces
+     *            {@link FunctionalGetGeographicalCoordinatesParametersWindow.SwingWorker}s 
+     *            for the {@link FunctionalGetGeographicalCoordinatesParametersWindow}s.
+     * @return {@code true} if {@code factory} was set as the factory that produces swingworkers for
+     *         the {@link FunctionalGetGeographicalCoordinatesParametersWindow}s; <br/>
+     *         {@code false} if there was a factory already set.
+     */
+    public static
+            boolean
+            setSwingWorkerFactory( SwingWorkerFactory< FunctionalGetGeographicalCoordinatesParametersWindow.SwingWorker, Iterable< SimpleAirship > > factory ) {
+        //TODO
+        synchronized (factoryLock) {
+            if( swFactory == null && factory != null ) {
+                swFactory = factory;
+                return true;
+            }
+            return false;
+        }
         
-        return new FunctionalGetWindowSwingWorker( airshipsDatabase, bodyPanel,
-                                                   functionalWindow.getErrorJTextArea() ) {
+    }
+        
+    // INNER CLASS
+    /**
+     * Class whose instances are {@link ExceptionHandlerSW} able to add funcitonality to a
+     * {@link FunctionalGetGeographicalCoordinatesParametersWindow}.
+     *
+     * @author Daniel Gomes, Eva Gomes, Gonçalo Carvalho, Pedro Antunes
+     */    
+    public static abstract class SwingWorker extends ExceptionHandlerSW< Iterable< SimpleAirship > > {
+        
+     // INSTANCE FIELDS
+        
+        //TODO
+        private JBodyPanelForMainWindow bodyPanel;
+        
+        /**
+         * String representation of the parameters to use in the commands and that are obtained from
+         * the window's text fields.
+         */
+        
+        // TextField Information
+        protected String latitude;
+        protected String longitude;
+        protected String airshipsNumber;
+        
+        // JLabel Information
+        
+        protected String latitudeLabel;
+        protected String longitudeLabel;
+        protected String airshipsNumberLabel;
+        
+     // CONSTRUCTOR
+        public SwingWorker( GetGeographicalCoordinatesParametersWindow window ) {
+        
+            super( window.getErrorJTextArea() );
             
-            /**
-             * String representation of the parameters to use in the commands and that are obtained
-             * from the window's text fields.
-             */
-            private String latitude = functionalWindow.getLatitude().getJTextField().getText();
-            private String longitude = functionalWindow.getLongitude().getJTextField().getText();
-            private String airshipsNumber = functionalWindow.getAirshipsNumber().getJTextField()
-                                                            .getText();
+            latitude = window.getLatitude().getJTextField().getText();
+            longitude = window.getLongitude().getJTextField().getText();
+            airshipsNumber = window.getAirshipsNumber().getJTextField().getText();
             
-            /**
-             * Implementation of the {@link SwingWorker#doInBackground() doInBackground()} method
-             * with the purpose the purpose of executing a
-             * {@link GetTheNearestAirshipsToGeographicPositionCommand} and obtaining its result.
-             * 
-             * @return Returns an {@code Iterable<Airship>} containing the specified number of
-             *         airships that are closer to the given geographic coordinates.
-             * 
-             * @throws NumberFormatException
-             *             If the String {@code airshipsNumber} cannot be converted to Integer or
-             *             the {@code latitude} and {@code longitude} cannot be converted to Double.
-             */
-            @Override
-            protected Iterable< Airship > doInBackground() throws Exception {
-                
-                return new GetTheNearestAirshipsToGeographicPositionCommand(
-                                                                             (InMemoryAirshipsDatabase)airshipsDatabase,
-                                                                             StringUtils.parameterToInteger( "Airships Number",
-                                                                                                             airshipsNumber ),
-                                                                             StringUtils.parameterToDouble( "Latitude",
-                                                                                                            latitude ),
-                                                                             StringUtils.parameterToDouble( "Longitude",
-                                                                                                            longitude ) ).call()
-                                                                                                                         .get();
-            }
+            latitudeLabel = window.getLatitude().getJLabel().getText();
+            longitudeLabel = window.getLongitude().getJLabel().getText();
+            airshipsNumberLabel = window.getAirshipsNumber().getJLabel().getText();
+        }
+        
+        // IMPLEMENTATION OF METHODS INHERITED FROM SwingWorker and ExceptionHandlerSW
+        /**
+         * Implementation of the {@link ExceptionHandlerSW#finalizeDone }.
+         * This method will receive the result of the
+         * {@link SwingWorker#doInBackground()} method and update 
+         * {@link JBodyPanelForMainWindow}, closing this window.
+         * 
+         * @param resultOfDoInBackGround
+         *            - The result of the {@link SwingWorker#doInBackground()}
+         *            method.
+         */ 
+        
+        @Override
+        protected void finalizeDone( Iterable< SimpleAirship > resultOfDoInBackGround )
+            throws Exception {
+        
+            bodyPanel.updateBodyPanel( resultOfDoInBackGround );
             
-            /**
-             * Override of the {@link FunctionalGetWindowSwingWorker#functionalDone()
-             * functionalDone()}. This method will receive the result of the
-             * {@link SwingWorker#doInBackground() doInBackground()} method and use it to update the
-             * body panel of the {@code MainWindow} and close this window.
-             * 
-             * @param resultOfDoInBackGround
-             *            - The result of the {@link SwingWorker#doInBackground() doInBackground()}
-             *            method.
-             */
-            @Override
-            public final void functionalDone( Iterable< Airship > resultOfDoInBackGround )
-                throws Exception {
-                
-                super.finalizeDone( resultOfDoInBackGround );
-                functionalWindow.dispose();
-            }
-        };
-    };
+        }
+        
+    }
+    
+    
 }
